0508a27e5595d63b16b90c012b1e824adb651fa4
v662-coder <83722974+v662-coder@users.noreply.github.com>
2026-08-27 14:24:23 +0530

    Fix: Add React import to App.jsx and main.jsx
    - Fixed 'React is not defined' error
    - Added missing React import statements
    - App now runs without ReferenceError

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-﻿import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+﻿import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { ItemsProvider } from './context/ItemsContext';
 import { ThemeProvider } from './context/ThemeContext';
--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
-import { AuthProvider } from "./context/AuthContext.jsx";
-import { ThemeProvider } from "./context/ThemeContext.jsx";
-import { ItemsProvider } from "./context/ItemsContext.jsx";
-import "./index.css";
+﻿import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider>
-        <AuthProvider>
-          <ItemsProvider>
-            <App />
-          </ItemsProvider>
-        </AuthProvider>
-      </ThemeProvider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
